bc0867846fde1b67a79518d8fcea443fc5daf9be
Sai Bharath <bollapallisaibharath@gmail.com>
2025-09-18 23:06:12 +0530

    Add files via upload

---
+++ com/tns/oops/MainEmployee.java
@@ -0,0 +1,12 @@
+package com.tns.oops.encapsulation;
+
+public class MainEmployee{
+    public static void main(String[] args) {
+        Employee emp=new Employee();
+        emp.setSid(101);
+        emp.setSname("Sai");
+        emp.display();
+    }
+
+}
+
